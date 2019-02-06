fa15eae1918c9ad19bf2a5bcadbfaa20b30881e4
Mario Gómez <mariogomezmtz@gmail.com>
2019-02-06 16:28:33 -0600

    Change production dockerfile to use multistage building

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,9 +1,9 @@
-#!/bin/bash
+#!/bin/sh
 set -e
 
 # Remove a potentially pre-existing server.pid for Rails.
-if [ -f /src/tmp/pids/server.pid ]; then
-  rm -f /src/tmp/pids/server.pid
+if [ -f /var/www/current/tmp/pids/server.pid ]; then
+  rm -f /var/www/current/tmp/pids/server.pid
 fi
 
 # Then exec the container's main process (what's set as CMD in the Dockerfile).
